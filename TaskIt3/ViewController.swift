34e0f32c8af3dba95eac1f5074b5996d9d15fa0f
delmarz <delmarz@delmarzs-Mac-Pro.local>
2015-01-18 18:46:27 +0800

    Initial Commit

---
+++ TaskIt3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIt3
+//
+//  Created by delmarz on 1/18/15.
+//  Copyright (c) 2015 delmarz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TaskIt3Tests/TaskIt3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TaskIt3Tests.swift
+//  TaskIt3Tests
+//
+//  Created by delmarz on 1/18/15.
+//  Copyright (c) 2015 delmarz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TaskIt3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
